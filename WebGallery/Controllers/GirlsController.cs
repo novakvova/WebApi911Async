68503447c8f074afd0ed9681f34c639737bbc305
novakvova <novakvova@gmail.com>
2021-04-25 13:25:06 +0300

    Add show girls

---
+++ WebGallery/Controllers/GirlsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGallery.Models;
+
+namespace WebGallery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GirlsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("search")]
+        public IActionResult SearchGrils()
+        {
+            var list = new List<GirlVM>()
+            {
+                new GirlVM
+                {
+                    Name = "Саша Маргалова",
+                    Age=33,
+                    Height=198,
+                    Weight=95,
+                    Image="1.png"
+                },
+                new GirlVM
+                {
+                    Name = "Наташа Ростова",
+                    Age=18,
+                    Height=202,
+                    Weight=102,
+                    Image = "2.jpg"
+                },
+            };
+            return Ok(list);
+        }
+        
+    }
+}
---
+++ WebGallery/Models/GirlVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGallery.Models
+{
+    public class GirlVM
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public string Image { get; set; }
+    }
+}
